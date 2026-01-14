cfb83154443a000469c96c280cb37ade843091fe
TriLuu01 <tluu@stevens.edu>
2026-01-15 00:26:13 +0700

    Initial Commit

---
+++ Vinnies/VinniesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinniesApp.swift
+//  Vinnies
+//
+//  Created by Tri Dep Trai on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct VinniesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
